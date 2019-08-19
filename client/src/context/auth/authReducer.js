1c8faf46294f34745b8a33c5655852fe0a8e15a3
jaqb8 <46377510+jaqb8@users.noreply.github.com>
2019-08-19 19:35:44 +0200

    implement user login

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -11,6 +11,7 @@ import {
 
 export default (state, action) => {
   switch (action.type) {
+    case LOGIN_SUCCESS:
     case REGISTER_SUCCESS:
       localStorage.setItem("token", action.payload.token);
       return {
@@ -19,6 +20,7 @@ export default (state, action) => {
         isAuthenticated: true,
         loading: false
       };
+    case LOGIN_FAIL:
     case AUTH_ERROR:
     case REGISTER_FAIL:
       localStorage.removeItem("token");
--- routes/auth.js
+++ routes/auth.js
@@ -42,16 +42,12 @@ router.post(
       let user = await User.findOne({ email });
 
       if (!user) {
-        return res
-          .status(400)
-          .json({ msg: "Invalid credentials. User does not exist." });
+        return res.status(400).json({ msg: "Invalid credentials." });
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
-        return res
-          .status(400)
-          .json({ msg: "Invalid credentials. Wrong password." });
+        return res.status(400).json({ msg: "Invalid credentials." });
       }
 
       const payload = {
